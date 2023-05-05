3a80d4cedc625ccfcd9db49f0a971c0932d1d4dc
Yousuf-Gazi <gaziyousufridoy@gmail.com>
2023-05-05 21:17:29 +0600

    feat: implemented get a user route
    In users.js I implemented get a user route where we can get a user by
    id.

--- routes/users.js
+++ routes/users.js
@@ -42,7 +42,17 @@ router.delete('/:id', async (req, res) => {
     }
 });
 
-// Get user
+// Get a user
+router.get('/:id', async (req, res) => {
+    try {
+        const user = await User.findById(req.params.id);
+        const { password, ...other } = user._doc;
+        res.status(200).json(other);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
 // Follow user
 // Unfollow user
 
